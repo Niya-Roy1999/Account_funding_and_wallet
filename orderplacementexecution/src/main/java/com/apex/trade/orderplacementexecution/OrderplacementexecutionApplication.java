30b02008d257026ee546ea00c7d3400a8a2c1f49
Kari Rakesh <kari.rakesh@apexon.com>
2025-09-18 16:22:02 +0530

    first commit

---
+++ orderplacementexecution/src/main/java/com/apex/trade/orderplacementexecution/OrderplacementexecutionApplication.java
@@ -0,0 +1,13 @@
+package com.apex.trade.orderplacementexecution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderplacementexecutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderplacementexecutionApplication.class, args);
+	}
+
+}
---
+++ orderplacementexecution/src/test/java/com/apex/trade/orderplacementexecution/OrderplacementexecutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.trade.orderplacementexecution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderplacementexecutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
